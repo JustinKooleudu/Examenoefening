34e68fef90576d4613e23903a69ca4365184e72a
Justin Kool <justin.kool06@gmail.com>
2023-09-18 14:37:35 +0200

    homepagina

---
+++ Index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <img
+            src="https://www.rijksoverheid.nl/binaries/content/gallery/rijksoverheid/channel-afbeeldingen/logos/beeldmerk-rijksoverheid-desktop.svg">
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li class="rechts"><a class="right" href="#news">News</a></li>
+                <li class="rechts"><a class="right" href="#about">About</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="partijen">
+        <button>
+            partij: 1
+        </button>
+        <button>
+            partij: 2
+        </button>
+        <button>
+            partij: 3
+        </button>
+        <button>
+            partij: 4
+        </button>
+        <button>
+            partij: 5
+        </button>
+        <button>
+            partij: 6
+        </button>
+        <button>
+            partij: 7
+        </button>
+        <button>
+            partij: 8
+        </button>
+        <button>
+            partij: 9
+        </button>
+        <button>
+            partij: 10
+        </button>
+        <button>
+            partij: 11
+        </button>
+        <button>
+            partij: 12
+        </button>
+    </div>
+</body>
+
+</html>
