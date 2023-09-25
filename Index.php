49589bee352cd7298cb1b4c103e5db22acefbf54
Justin Kool <justin.kool06@gmail.com>
2023-09-25 11:26:08 +0200

    help

--- Index.php
+++ Index.php
@@ -4,8 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <title>totaal NIET de rijksoverheid</title>
+    <title>Document</title>
 </head>
 
 <body>
@@ -20,44 +19,6 @@
             </ul>
         </nav>
     </header>
-
-    <div class="partijen">
-        <button>
-            partij: 1
-        </button>
-        <button>
-            partij: 2
-        </button>
-        <button>
-            partij: 3
-        </button>
-        <button>
-            partij: 4
-        </button>
-        <button>
-            partij: 5
-        </button>
-        <button>
-            partij: 6
-        </button>
-        <button>
-            partij: 7
-        </button>
-        <button>
-            partij: 8
-        </button>
-        <button>
-            partij: 9
-        </button>
-        <button>
-            partij: 10
-        </button>
-        <button>
-            partij: 11
-        </button>
-        <button>
-            partij: 12
-        </button>
-    </div>
 </body>
+
 </html>
---
+++ Stem.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>totaal NIET de rijksoverheid</title>
+</head>
+
+<body>
+    <header>
+        <img
+            src="https://www.rijksoverheid.nl/binaries/content/gallery/rijksoverheid/channel-afbeeldingen/logos/beeldmerk-rijksoverheid-desktop.svg">
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li class="rechts"><a class="right" href="#Over ons">Over ons</a></li>
+                <li class="rechts"><a class="right" href="#Stem">Stem</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="partijen">
+        <button class="VVD"></button>
+        <button class="D66"></button>
+        <button class="">
+            partij: 3
+        </button>
+        <button class="">
+            partij: 4
+        </button>
+        <button class="">
+            partij: 5
+        </button>
+        <button class="">
+            partij: 6
+        </button>
+        <button class="">
+            partij: 7
+        </button>
+        <button class="">
+            partij: 8
+        </button>
+        <button class="">
+            partij: 9
+        </button>
+        <button class="">
+            partij: 10
+        </button>
+        <button class="">
+            partij: 11
+        </button>
+        <button class="">
+            partij: 12
+        </button>
+    </div>
+</body>
+</html>
